bd2b2e4406ece36919e31256948040ea58697d45
Igor Araújo <igorgabsprofissional@gmail.com>
2026-03-02 16:53:28 -0300

    feat: Componentization of songs in the playlist to avoid overly large code and improve organization

--- src/lib/components/profile/user-playlists/UserSelectedPlaylistModal.svelte
+++ src/lib/components/profile/user-playlists/UserSelectedPlaylistModal.svelte
@@ -4,6 +4,7 @@
 
 	// Components
 	import ExternalLink from '$lib/components/general/ExternalLink.svelte';
+	import MusicFromPlaylist from './MusicFromPlaylist.svelte';
 
 	// Stores
 	import { translationsStore } from '$lib/stores/translations.store';
@@ -55,12 +56,6 @@
 		}
 
 		isLoading = false;
-	}
-
-	function formatDuration(ms: number) {
-		const minutes = Math.floor(ms / 60000);
-		const seconds = Math.floor((ms % 60000) / 1000);
-		return `${minutes}:${String(seconds).padStart(2, '0')}`;
 	}
 </script>
 
@@ -155,54 +150,7 @@
 				{:else}
 					{#each musicsFromPlaylist as music}
 						{#if music.track.external_urls?.spotify}
-							<a
-								class="group flex w-full cursor-pointer gap-4 rounded-lg px-4 py-3 text-left transition hover:bg-s-muted"
-								href={music.track.external_urls.spotify}
-								target="_blank"
-								rel="noopener noreferrer"
-							>
-								{#if music.track.album.images?.[2]?.url}
-									<img
-										src={music.track.album.images[2].url}
-										alt={music.track.name}
-										class="h-14 w-14 shrink-0 rounded object-cover"
-									/>
-								{/if}
-
-								<div class="min-w-0 flex-1">
-									<div class="flex items-center justify-between gap-4">
-										<div class="min-w-0">
-											{#if music.track.name}
-												<p
-													class="truncate text-sm font-medium text-t-primary transition group-hover:text-brand-primary"
-												>
-													{music.track.name}
-												</p>
-											{/if}
-
-											{#if music.track.artists}
-												<p class="truncate text-xs text-t-secondary">
-													{music.track.artists.map((a: { name: string }) => a.name).join(', ')}
-												</p>
-											{/if}
-										</div>
-
-										{#if music.track.duration_ms}
-											<span class="shrink-0 text-xs text-t-secondary">
-												{formatDuration(music.track.duration_ms)}
-											</span>
-										{/if}
-									</div>
-
-									{#if music.added_by?.id && music.added_at}
-										<p class="mt-1 text-[11px] text-t-muted">
-											{$translationsStore.profilePage.profilePageUserSelectedPlaylistModalAddedBy}
-											{music.added_by.id} •{' '}
-											{new Date(music.added_at).toLocaleDateString()}
-										</p>
-									{/if}
-								</div>
-							</a>
+							<MusicFromPlaylist {music} />
 						{/if}
 					{/each}
 				{/if}
